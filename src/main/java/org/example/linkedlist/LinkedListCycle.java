b8f2bbf530c955dbbe8220499530ba3193395efb
Ali Hamzayev <alihmzyv@gmail.com>
2023-07-04 10:27:15 +0400

    solve "Linked List Cycle".

---
+++ src/main/java/org/example/linkedlist/LinkedListCycle.java
@@ -0,0 +1,20 @@
+package org.example.linkedlist;
+
+import org.example.common.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> traversed = new HashSet<>(10000);
+        while (head != null) {
+            if (traversed.contains(head))
+                return true;
+            else
+                traversed.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
