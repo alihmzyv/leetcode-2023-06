049cb9d2fb11532a20f237126e7af83b7169c9e5
Ali Hamzayev <alihmzyv@gmail.com>
2023-07-05 10:14:44 +0400

    solve "Remove Linked List Elements"

--- src/main/java/org/example/linkedlist/PalindromeLinkedList.java
+++ src/main/java/org/example/linkedlist/PalindromeLinkedList.java
@@ -22,4 +22,8 @@ public class PalindromeLinkedList {
             }
             return true;
     }
+
+    /*
+    TODO: study other approaches
+     */
 }
---
+++ src/main/java/org/example/linkedlist/RemoveLinkedListElements.java
@@ -0,0 +1,26 @@
+package org.example.linkedlist;
+
+import org.example.common.ListNode;
+
+public class RemoveLinkedListElements {
+    /*
+    own solution, time = O(n), space = O(1)
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.val == val) {
+                head = head.next;
+                curr = curr.next;
+            } else {
+                ListNode next = curr.next;
+                if (next != null && next.val == val) {
+                    curr.next = next.next;
+                } else {
+                    curr = next;
+                }
+            }
+        }
+        return head;
+    }
+}
